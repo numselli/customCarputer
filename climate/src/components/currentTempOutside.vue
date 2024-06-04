edd17b8573a3db5baed164303aca634f54550abc
numselli <58607232+numselli@users.noreply.github.com>
2024-06-04 16:27:14 -0700

    start work on climate app

---
+++ climate/src/components/currentTempOutside.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      temp: 0
+    }
+  },
+  methods: {
+    updateTemp(){
+        this.temp=Math.floor(Math.random() * 35);
+    }
+  },
+  mounted(){
+    setInterval(this.updateTemp, 30*1000)
+  }
+}
+</script>
+<template>
+  <div>
+    <div>
+      <span class="material-symbols-outlined align-middle">filter_drama</span>
+      <!-- <span>outside temp:</span> -->
+      <span class="align-middle">{{ temp }}</span>
+    </div>
+  </div>
+</template>
