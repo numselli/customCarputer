d4ffda3493881f041c646b038688fc28196fc1b4
numselli <58607232+numselli@users.noreply.github.com>
2024-06-30 10:37:19 -0700

    Update index.mjs

--- canbus/index.mjs
+++ canbus/index.mjs
@@ -3,12 +3,41 @@ import * as can from "socketcan";
 const channel = can.createRawChannel("can0", true);
 
 const IDs = new Map()
+// drive mode
 IDs.set(288, {
-    id: 288,
+    lastData: "" 
+})
+// brake position
+IDs.set(48, {
+    lastData: ""
+})
+// hv SOC
+IDs.set(971, {
+    lastData: ""
+})
+// evmode actave
+IDs.set(1321, {
+    lastData: ""
+})
+// tank level
+IDs.set(1444, {
+    lastData: ""
+})
+// kmph
+IDs.set(970, {
+    lastData: ""
+})
+// Throttle postion
+IDs.set(580, {
     lastData: ""
 })
 
-channel.addListener("onMessage", function (msg) { 
+// 1480 cuse controll actave
+channel.addListener("onMessage", function (msg) {
+    let json = JSON.stringify(msg.data);
+    let buginal = JSON.parse(json).data
+    
+    // if (msg.id === 1389) return console.log(buginal)
     if (IDs.has(msg.id)){
         const id = IDs.get(msg.id)
 
@@ -28,7 +57,6 @@ channel.addListener("onMessage", function (msg) {
 function changedData(msg){
     switch(msg.id){
         case 288:{
-
             switch (msg.bufferOriginal[msg.bufferOriginal.length-1]){
                 case 77:{
                     console.log("car in park")
@@ -47,7 +75,25 @@ function changedData(msg){
                 }break;
             }
 
-        }break
+        }break;
+        // case 48:{
+        //     console.log(`Brake Pedal Position: ${((msg.bufferOriginal[4]/127)*100).toFixed(2)}%`)
+        // }break;
+        case 971: {
+            console.log(`HV SOC: ${msg.bufferOriginal[6]}%`)
+        }break;
+        case 1321: {
+            console.log(msg.bufferOriginal[4] === 0 ? "evmode off" : "evmode on")
+        }break;
+        case 1444: {
+            console.log(`gas tank: ${((msg.bufferOriginal[1]/44)*100).toFixed(2)}%`)
+        }break;
+        case 970: {
+            console.log(`${msg.bufferOriginal[2]}KM/h`)   
+        }break;
+        case 580: {
+            console.log(`Throttle Pedal Position: ${((msg.bufferOriginal[6]/200)*100).toFixed(2)}%`)
+        }break;
     }
 }
 
