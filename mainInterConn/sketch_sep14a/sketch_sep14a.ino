fc27ea7504811a38f67ad7b2423fc6cd0f1a30da
numselli <58607232+numselli@users.noreply.github.com>
2024-08-23 04:49:02 -0700

    create mainInterConn

---
+++ mainInterConn/sketch_sep14a/sketch_sep14a.ino
@@ -0,0 +1,28 @@
+void setup(){
+  Serial.begin(9600);
+}
+
+int averagePinResponce(int pin){
+  int reading = 0;
+  int count = 10;
+  for (int index = 0; index < count; index++) {
+    reading += analogRead(pin);
+  }
+
+  return (reading/count);
+}
+
+void loop(){
+  int btnOneReading = averagePinResponce(A6);
+  int btnTwoReading = averagePinResponce(A5);
+
+  if (!(btnOneReading >= 501 && btnOneReading <= 521)) {
+    Serial.println(String(btnOneReading)+"-"+String(btnTwoReading));
+  }
+
+  if (!(btnTwoReading >= 501 && btnTwoReading <= 521)) {
+    Serial.println(String(btnOneReading)+"-"+String(btnTwoReading));
+  }
+
+  delay(200);
+}
