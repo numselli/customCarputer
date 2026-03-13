ad7401258b3a91b68f1bcd0757af8aeb311d0a4f
numselli <58607232+numselli@users.noreply.github.com>
2026-03-13 15:33:37 -0700

    Update service.mjs

--- dispatcher/service.mjs
+++ dispatcher/service.mjs
@@ -12,30 +12,24 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data) {
     const parsedData = JSON.parse(data)
-    switch (parsedData.catagory){
-      case "navi":{
-        switch(parsedData.state){
-          case "start":{
+    switch (parsedData.catagory) {
+      case "navi": {
+        switch (parsedData.state) {
+          case "start": {
             execCommand("playerctl pause")
-          }break;
-          case "stop":{
+          } break;
+          case "stop": {
             execCommand("playerctl play")
-          }break;
+          } break;
         }
-      }break;
-      default:{
+      } break;
+      default: {
         console.log('received: %s', data);
       }
     }
   });
 
 });
-
-function brodcastWSS(data){
-  wss.clients.forEach(ws=>{
-    ws.send(data)
-  })
-}
 
 // serial
 // steeringWheelControls
@@ -50,45 +44,42 @@ parser.on('data', async (data) => {
 
   if (moddedData.includes("-")) {
     const readings = moddedData.split("-")
-  
+
     const sw0 = Number(readings[0])
     const sw1 = Number(readings[1])
-  
-    if (sw0 >= 1013 && sw0 <= 1014) {
-        const volumeMuted = (await execCommand("pactl get-sink-mute @DEFAULT_SINK@")).includes("yes")
-  
-        if (volumeMuted) {
-            await execCommand("pactl set-sink-mute @DEFAULT_SINK@ 0")
-            await execCommand("pactl set-sink-volume @DEFAULT_SINK@ 0")
-        }
-  
-        execCommand("pactl set-sink-volume @DEFAULT_SINK@ +5%")
-    } else if (sw0 >= 992 && sw0 <= 994) {
-        await execCommand("pactl set-sink-volume @DEFAULT_SINK@ -5%")
-  
-        const volumeLevels = (await execCommand("pactl get-sink-volume @DEFAULT_SINK@"))
-        const volumeLevelArray = volumeLevels.split("/")
-  
-        const leftVolume = volumeLevelArray[1].trim()
-        const rightVolume = volumeLevelArray[3].trim()
-  
-        if (leftVolume === "0%" && rightVolume === "0%") await execCommand("pactl set-sink-mute @DEFAULT_SINK@ 1")
-    } else if (sw0 >= 1019 && sw0 <= 1020) {
-        execCommand("playerctl previous")
+
+    if (sw0 == 930) {
+      const volumeMuted = (await execCommand("pactl get-sink-mute @DEFAULT_SINK@")).includes("yes")
+
+      if (volumeMuted) {
+        await execCommand("pactl set-sink-mute @DEFAULT_SINK@ 0")
+        await execCommand("pactl set-sink-volume @DEFAULT_SINK@ 0")
+      }
+
+      execCommand("pactl set-sink-volume @DEFAULT_SINK@ +5%")
+    } else if (sw0 == 780) {
+      await execCommand("pactl set-sink-volume @DEFAULT_SINK@ -5%")
+
+      const volumeLevels = (await execCommand("pactl get-sink-volume @DEFAULT_SINK@"))
+      const volumeLevelArray = volumeLevels.split("/")
+
+      const leftVolume = volumeLevelArray[1].trim()
+      const rightVolume = volumeLevelArray[3].trim()
+
+      if (leftVolume === "0%" && rightVolume === "0%") await execCommand("pactl set-sink-mute @DEFAULT_SINK@ 1")
+    } else if (sw0 >= 990 && sw0 <= 991) {
+      execCommand("playerctl previous")
     } else if (sw0 >= 1022 && sw0 <= 1023) {
-        execCommand("playerctl next")
-    } else if (!(sw0 >= 501 && sw0 <= 521)) {
-        console.log(`err btnOneReading: ${sw0}`)
+      execCommand("playerctl next")
+    } else {
+      console.log(`err btnOneReading: ${sw0}`)
     }
-  
+
     if (sw1 >= 1022 && sw1 <= 1024) {
-        execCommand("playerctl play-pause")
-    } else if (!(sw1 >= 501 && sw1 <= 521)) {
-        console.log(`err btnTwoReading: ${sw1}`)
+      execCommand("playerctl play-pause")
+    } else {
+      console.log(`err btnTwoReading: ${sw1}`)
     }
-  } else {
-    brodcastWSS(`car-${moddedData}`)
-    console.log(moddedData)
   }
 })
 
